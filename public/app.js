4272a05dbf22be44cee97cf0ff7e27a1c49c407f
Marvinmind <foaly02@outlook.com>
2015-10-22 13:34:10 +0200

    Tried directives
    First tries to introduce new angular html code through directives

--- public/app.js
+++ public/app.js
@@ -1,11 +1,23 @@
 var app = angular.module('myApp',[]);
-app.controller('sectionController', function($scope, $http){
+app.controller('sectionController', ['$scope','$sce','$http', function($scope, $sce, $http){
 	console.log('hey y');
 	$http.get("sections.js").success(function(result){
 		$scope.sections = result.sections;
 	});
-	function loadSection(x){
-		console.log(x);
-		return x.sectionName;
+	$scope.loadSection = function(x){
+		return $sce.trustAsHtml(x.sectionName+" <button ng-click='stuff()'>stuff</button>");
 	}
-});
+	$scope.loadChildren = function(subsections){
+		console.log(subsections);
+	}
+	$scope.stuff = function(){console.log('hey')};
+}]);
+
+app.directive('addChildren', []function($compile){
+	return {
+		restrict: 'E',
+		scope: {
+			id: '@addChildren'
+		}
+	}
+})
