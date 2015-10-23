c3c99c485a9c508fe5496d1b8ab5d94c4a594edf
Marvinmind <foaly02@outlook.com>
2015-10-24 00:29:49 +0200

    Load subsections
    The app loads the root section, extracts its subsections, loads those
    and displays with ng-repeat.

--- public/sections.js
+++ public/sections.js
@@ -1,21 +1,21 @@
 {"sections":[
 {
-"sectionId": "0",
-"sectionName": "root",
-"choose": "all",
-"minCP":120,
-"subsections": ["1","2","3"]
+	"sectionId": "0",
+	"sectionName": "root",
+	"choose": "all",
+	"minCP":120,
+	"subsections": ["1","2","3"]
 },
 
 {
-"sectionID": "1",
-"sectionName": "Fachstudium",
-"choose": "all",
-"minCP":30
+	"sectionId": "1",
+	"sectionName": "Fachstudium",
+	"choose": "all",
+	"minCP":30
 },
 
 {
-	"sectionID":"2",
+	"sectionId":"2",
 	"sectionName":"Professionalisierungsbereich",
 	"minCP":60,
 	"choose": "all",
@@ -23,19 +23,19 @@
 
 },
 {
-	"sectionID" : "2.0",
+	"sectionId" : "2.0",
 	"sectionName" : "Studienschwerpunkt",
 	"choose" : "one",
 	"subsections" : ["2.0.0","2.0.1","2.0.2","2.0.3","2.0.4","2.0.5","2.0.6","2.0.7","2.0.8","2.0.9"]
 },
 {
-	"sectionID" : "2.1",
+	"sectionId" : "2.1",
 	"sectionName" : "Schlüsselkompetenzen",
 	"minCP" : 12
 },
 
 {
-	"sectionID" : "3",
+	"sectionId" : "3",
 	"sectionName": "Masterarbeit",
 	"minCP": 30
 }
--- public/withcompile/app.js
+++ public/withcompile/app.js
@@ -1,12 +1,19 @@
 var app = angular.module('myApp',[]);
 app.controller('standardController', ['scope', function($scope){
-	//$scope.surname='martin';
+	$scope.surname='martin';
 }]);
 
 app.factory('sectionService', ['$rootScope', '$http', function($rootScope, $http){
 	var sectionLocation = 'http://localhost:3000/sections.js';
 	function loadSectionById(id, callback){
-		$http.get(sectionLocation).success(function(data){console.log(data);});
+		$http.get(sectionLocation).success(function(data){
+			data.sections.forEach(function(section){
+				if (section.sectionId==id){
+					console.log(section);
+					callback(section);
+				}
+			});
+		;});
 	}
 	return {loadBySectionId: loadSectionById};
 }]);
@@ -16,14 +23,20 @@ app.directive('sectionloader',['$compile', 'sectionService',function($compile, s
 	return	{
 		restrict : 'E',
 		template: '<button>loadSection</button><br>',
-		scope : {},
+		scope : {data:'@'},
 		link: function link(scope, element, attrs) {
 			element.bind('click', function(){
 		    	sectionService.loadBySectionId(0, function(data){
-		    		element.append($compile('<input ng-model="myinput"></input><br><p>{{myinput}}</p>')(scope));
+					scope.section=data;
+					scope.subsections = [];
+					data.subsections.forEach(function(id){
+						sectionService.loadBySectionId(id,function(data){
+							scope.subsections.push(data);
+						});
+					});
+		    		element.append($compile('<div class="subsection"><ul><li ng-repeat="subsection in subsections">{{subsection.sectionName}}</li><ul></div>')(scope));
 		    	});
 			});
-		    //insertText();
 		}
 	}
 }]);
