e64b6d9735ed6759a9ad84e8bd0991404ef47d62
Martin Schwarzmaier <martin.schwarzmaier@gmail.com>
2015-10-24 23:24:26 +0200

    working recursion

--- public/withcompile/app.js
+++ public/withcompile/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp',[]);
-app.controller('standardController', ['scope', function($scope){
+app.controller('standardController', ['$rootScope', 'sectionService' ,function($scope, sectionService){
 	$scope.surname='martin';
 }]);
 
@@ -13,30 +13,59 @@ app.factory('sectionService', ['$rootScope', '$http', function($rootScope, $http
 					callback(section);
 				}
 			});
-		;});
+		});
 	}
 	return {loadBySectionId: loadSectionById};
 }]);
 
-app.directive('sectionloader',['$compile', 'sectionService',function($compile, sectionService){
+app.directive('sectionloader',['$compile', 'sectionService', '$http', 
+	function($compile, sectionService, $http){
 	
 	return	{
 		restrict : 'E',
-		template: '<button>loadSection</button><br>',
-		scope : {data:'@'},
-		link: function link(scope, element, attrs) {
-			element.bind('click', function(){
-		    	sectionService.loadBySectionId(0, function(data){
-					scope.section=data;
-					scope.subsections = [];
-					data.subsections.forEach(function(id){
-						sectionService.loadBySectionId(id,function(data){
-							scope.subsections.push(data);
-						});
-					});
-		    		element.append($compile('<div class="subsection"><ul><li ng-repeat="subsection in subsections">{{subsection.sectionName}}</li><ul></div>')(scope));
-		    	});
-			});
+		template: '<button ng-click="insertSubsections()">loadSection</button><br>',
+		scope : {sectionId:'@'}, 
+		terminal: false,
+		compile: function (element, attrs) {
+			return function(scope, iElement, iAttr){
+		    	scope.insertSubsections = function(){
+		    				    				    		console.log(iAttr)
+
+		    				    		console.log(iAttr.sectionId)
+
+		    		sectionService.loadBySectionId(iAttr.sectionId, 
+		    			function(data){
+		    				console.log(data);
+
+				    		var sectionHtmlLocation = "http://localhost:3000/withcompile/sectionhtml.html"
+							scope.section=data;
+							scope.subsections = [];
+							data.subsections.forEach(function(id){
+								sectionService.loadBySectionId(id,function(data){
+									scope.subsections.push(data);
+								});
+							});
+							$http.get(sectionHtmlLocation).success(function(data){
+					    		iElement.append(data);
+					  			$compile(iElement.contents())(scope);
+				  			});
+		    			});
+		    	};
+			};
 		}
 	}
 }]);
+/*
+app.directive('playground', [function($scope){
+	return{
+		restrict: 'E',
+		template: '
+					<div class="sectioncontainer">
+						<h2>{{sectionName}}</h2><br>
+						subsection:<br>
+						<ul>
+							<li ng-repeat="section in {{subsections}}"><playground></li>'
+
+
+	}
+}]);*/
