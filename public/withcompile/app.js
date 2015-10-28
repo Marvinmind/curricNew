7f9b1d3acfbe93523cc7afa6d5f12b1dc2f80e50
Martin Schwarzmaier <martin.schwarzmaier@gmail.com>
2015-10-28 19:00:04 +0100

    counting works to a degree, removing of modules not implemented

--- public/withcompile/app.js
+++ public/withcompile/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('myApp',[]);
 app.controller('standardController', ['$rootScope', 'sectionService' ,function($scope, sectionService){
-	$scope.chosenModules = {};
+	$scope.chosenModules = [];
 	$scope.getCpSum = function(){
 		var sum = 0;
 		if($scope.chosenModules){
@@ -8,8 +8,20 @@ app.controller('standardController', ['$rootScope', 'sectionService' ,function($
 				sum += module.creditPoints;
 			})
 		}
+		return sum;
 	}
-	$scope.test = function(){};
+	$scope.chooseModule = function(module, section){
+			var found = false;
+			$scope.chosenModules.forEach(function(chosenModule){
+				if(module.moduleId == chosenModule.moduleId){
+					chosenModule.sectionId = module.sectionId;
+					found = true;
+				}
+			});
+			if(found == false){
+				$scope.chosenModules.push({"moduleId":module.moduleId, "creditPoints":module.creditPoints, "sectionId":section.sectionId});
+			}
+	};
 }]);
 
 app.factory('sectionService', ['$rootScope', '$http', function($rootScope, $http){
@@ -41,23 +53,43 @@ app.factory('sectionService', ['$rootScope', '$http', function($rootScope, $http
 			};
 }]);
 
-app.directive('sectionloader',['$compile', 'sectionService', '$http', 
-	function($compile, sectionService, $http){
+app.directive('sectionloader',['$compile', 'sectionService', '$http','$rootScope',
+	function($compile, sectionService, $http, $rootScope){
 	
 	return	{
 		restrict : 'E',
 		template: '<button ng-click="insertSubsections()">loadSection</button><br>',
-		//scope : {sectionId:'@'}, 
+		scope : {}, 
 		terminal: false,
 		link: 
 			function(scope, iElement, iAttr){
-		    	scope.insertSubsections = function(){	    				    				    	
+		    	scope.insertSubsections = function(){
+		    		scope.moduleChecks = {};
+		    		scope.modules = []
+		    		$rootScope.$watchCollection("chosenModules", function(){
+		    			$rootScope.chosenModules.forEach(function(chosenModule){
+		    				scope.modules.forEach(function(module){
+		    					console.log(chosenModule);
+		    					if(chosenModule.sectionId != scope.section.sectionId && chosenModule.moduleId == module.moduleId){
+									scope.moduleChecks.moduleId = false;
+		    					}
+		    				});
+		    			});
+		    		});
+					scope.$watchCollection("moduleChecks",function(){
+						if(scope.modules){
+						scope.modules.forEach(function(module){
+							if(scope.moduleChecks[module.moduleName]==true)
+								$rootScope.chooseModule(module, scope.section);
+						});
+						}
+					});	    				    	
 		    		sectionService.loadBySectionId(iAttr.sectionId, function(data){
+		    			scope.section=data;
 						var sectionHtmlLocation = "http://localhost:3000/withcompile/sectionhtml.html";
 						var moduleHtmlLocation = "http://localhost:3000/withcompile/modulehtml.html"
-						scope.section=data;
-						if(data.subsections){
-							scope.subsections = [];
+
+						if(data.subsections){							scope.subsections = [];
 							data.subsections.forEach(function(id){
 								sectionService.loadBySectionId(id,function(data){
 									scope.subsections.push(data);
@@ -69,7 +101,6 @@ app.directive('sectionloader',['$compile', 'sectionService', '$http',
 							});
 						}
 						else{
-							scope.modules = [];
 							data.modules.forEach(function(id){
 								sectionService.loadByModuleId(id, function(data){
 									scope.modules.push(data);
